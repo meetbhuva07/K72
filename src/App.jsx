7e099b9ecb585a82f836f57710aad5e84454dd4d
meetbhuva07 <meetbhuva0009@gmail.com>
2026-01-10 23:24:49 +0530

    Today I started making a navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import { Link, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Project from './pages/Project'
+import Navbar from './components/navigation/Navbar'
 
 
 const App = () => {
 
   return (
-    <div className='text-white'>
-
-    
-
+    <div >
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/agence' element={<Agence />} />
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -60,7 +60,7 @@ const Agence = () => {
         <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>SEVEN7Y <br />
           TWO</h1>
       </div>
-      <div className="pl-[40%] mt-20">
+      <div className="pl-[40%] mt-">
         <p className='text-6xl '>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
       </div>
     </div>
