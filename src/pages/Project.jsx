1ca017269fae0ee1410a1280899d569cf2eed90b
meetbhuva07 <meetbhuva0009@gmail.com>
2026-01-25 19:20:57 +0530

    .

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -8,8 +8,8 @@ import gsap from 'gsap'
 const Project = () => {
 const project = [{ 
   image1 :'https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b',
-  image2 :'https://k72.ca/images/caseStudies/COUP_FUMANT/CF_thumbnail.jpg?w=1280&h=960&s=c119303a20520c4188aa3f592038fd4c'}, {
-  image1 :'https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail.jpg?w=1280&h=960&s=650a04dfc31ad85bfc64c0ddccc83f1e',
+  image2 :'https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail.jpg?w=1280&h=960&s=650a04dfc31ad85bfc64c0ddccc83f1e'}, {
+  image1 :'https://k72.ca/images/caseStudies/COUP_FUMANT/CF_thumbnail.jpg?w=1280&h=960&s=c119303a20520c4188aa3f592038fd4c',
   image2 :'https://k72.ca/images/caseStudies/OKA/OKA_thumbnail.jpg?w=1280&h=960&s=c12c27c9db3c521e4c82a246a8d5c022'}, {
   image1 :'https://k72.ca/images/caseStudies/SHELTON/thumbnailimage_shelton.jpg?w=1280&h=960&s=63d0eaa180cbc02d3ada285ad9ef1479',
   image2 :'https://k72.ca/images/caseStudies/BEST/BEST_site_Thumbnail.jpg?w=1280&h=960&s=2b73eecfda8d95a72efa768383b50860'}, {
@@ -30,7 +30,7 @@ useGSAP(function(){
       trigger:'.lol',
       markers:true,
       start:'top 100%',
-      end:-'top -150%',
+      end:'top -150%',
       scrub:true
       
     }
@@ -40,10 +40,10 @@ useGSAP(function(){
   return (
     <div className='p-4 mb-[100vh]'>
       <div className=" pt-[45vh]">
-        <h2 className='font-[font2] text-[9.5vw] pb-3  uppercase'>projets</h2>
+        <h2 className='font-[font2] text-[9.5vw]   uppercase'>projets</h2>
       </div>
       <div className='-mt-20 lol '>
-        {project.map(function(elem,idx){
+        {project.map(function(elem, idx){
           return  <div key={idx} className='hero w-full h-200 flex mb-4 gap-4  '>
           <ProjectCard image1 = {elem.image1} image2 = {elem.image2} />
           </div>
